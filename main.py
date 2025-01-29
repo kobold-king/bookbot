4091af348d4afc900b02513b738366467e21cfe7
kobold-king <nick.ragona@gmail.com>
2025-01-28 18:11:23 -0700

    added word count to main

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 def main():
     book_path ="books/frankenstein.txt"
     text = get_book_text(book_path)
+    word_count = get_word_count(text)
     print(text)
+    print(f" Total words =", word_count)
 
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+
+
+def get_word_count(book):
+    words = book.split()
+    return len(words)
+
 
 main()
